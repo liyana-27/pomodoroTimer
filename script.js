639c95788cbeed1f9e43e954864737ddf493b296
Liyana <liyana.zuhur@gmail.com>
2024-09-18 20:13:18 +0530

    Adding To Do

--- script.js
+++ script.js
@@ -102,3 +102,40 @@ stopBtn.addEventListener("click", () => {
         clearInterval(myInterval)
     }
 })
+
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+    if (inputBox.value === '') {
+        alert("Empty tasks cannot be added!");
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value =  "";
+    saveData();
+}
+
+listContainer.addEventListener("click", function(e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+    } else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showList() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showList();
